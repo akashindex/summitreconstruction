75c7a08d3a81e153b51ff70908a17486cf9bb970
akashindex <itz.asdf303@gmail.com>
2025-02-17 01:32:39 +0500

    chaasdass

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from "./component/navbar/Navbar"; 
-import Footer from './component/footer/Footer';
-import Home from "./component/home/Home";
-import Reconstruction from './component/Reconstruction/Reconstruction';
-import TestmonialComp from './component/testmonialComp/TestmonialComp';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Homepage"; 
+import Services from "./pages/Services";
 
 function App() {
-
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Reconstruction />
-      <TestmonialComp />
-      <Footer />
-    </>
+    <Router>
+      <Layout> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Services" element={<Services />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
-export default App
+export default App;
